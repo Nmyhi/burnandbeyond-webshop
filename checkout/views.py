cb5602057ddb0a1d48737491e17aae473904c2c4
Mihaly Lovrencsics <lomysa@gmail.com>
2024-02-06 22:28:41 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OYQqGJnRa2R7BliS6RiWdkUaCOvgjuUV7c3v5HoVMIsisPGOYkhmOQhNRtMMHbcoTnZXhQ2aBKONfoRGBNyI8Yg00zEGcrju1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
