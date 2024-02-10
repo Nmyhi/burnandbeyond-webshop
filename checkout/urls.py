f9ac9b1f9857bc8bac751520a903591cb31ec036
Mihaly Lovrencsics <lomysa@gmail.com>
2024-02-10 19:00:10 +0000

    Add checkout success view, logic and template

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,5 @@ from . import views
 # home app urls
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
