80eaf052e555d4d12c808f031ce9274006efb7ed
Mihaly Lovrencsics <lomysa@gmail.com>
2024-02-12 19:51:03 +0000

    Update webhook handler to handle profile update

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -39,6 +40,23 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save info was checked
+        profile = None
+        username = intent.metadata.username
+        # Only authenticated users
+        if username != 'AnonymusUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile . default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
+
         order_exists = False
         # create variable attempt for creating a delay
         attempt = 1
@@ -47,6 +65,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
+                    user_profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
